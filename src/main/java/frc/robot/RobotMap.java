6c5d1a1a7052a133fa8db6e781a95c6ec4a04bc1
yuvalLevy1 <Yuval@DESKTOP-9EPK839>
2019-01-19 16:20:48 +0200

    deleted the underscore

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,10 +9,10 @@ package frc.robot;
 
 public class RobotMap {
 	public interface CAN {
-		public static final int MOTOR_LEFT_1 = 0;
-		public static final int MOTOR_LEFT_2 = 1;
-		public static final int MOTOR_RIGHT_1 = 2;
-		public static final int MOTOR_RIGHT_2 = 3;
+		public static final int MOTOR_LEFT1 = 0;
+		public static final int MOTOR_LEFT2 = 1;
+		public static final int MOTOR_RIGHT1 = 2;
+		public static final int MOTOR_RIGHT2 = 3;
 	}
 
 	public interface DIO {
