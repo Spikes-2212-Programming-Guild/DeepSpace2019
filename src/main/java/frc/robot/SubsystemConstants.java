805be5dd8b0bd990623dd10f895a0670f2ef1ba2
YuvalLevy1 <Yuval213711062>
2019-01-19 19:51:30 +0200

    added constants for all of the subsystem.

--- src/main/java/frc/robot/SubsystemConstants.java
+++ src/main/java/frc/robot/SubsystemConstants.java
@@ -7,26 +7,36 @@ import java.util.function.Supplier;
 public class SubsystemConstants {
 
     public static class Drivetrain {
-
+        public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Drivetrain Speed",
+                0.1);
     }
 
     public static class Lift {
-
+        public static final Supplier<Double> STAYING_SPEED = ConstantHandler.addConstantDouble("Lift Staying Speed",
+                0.1);
+        public static final Supplier<Double> UP_SPEED = ConstantHandler.addConstantDouble("Lift up Speed",
+                0.1);
     }
+    public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Lift down Speed",
+            0.1);
 
     public static class Arm {
-
+        public static final Supplier<Double> ARM_SPEED = ConstantHandler.addConstantDouble("Arm Speed",
+                0.1);
     }
 
     public static class Lock {
-
+        public static final Supplier<Double> LOCK_SPEED = ConstantHandler.addConstantDouble("Lock Speed",
+                0.1);
     }
 
     public static class Latch {
-
+        public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Lift down Speed",
+                0.1);
     }
 
     public static class Roller {
-
+        public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Lift down Speed",
+                0.1);
     }
 }
