e4788203b886c95887ae623e39028a5968ff75a4
YuvalLevy1 <Yuval213711062>
2019-01-19 18:37:42 +0200

    added getTeamNumber and fixed the OI

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,7 +22,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         oi = new OI();
-        drivetrain = new TankDrivetrain(new InvertedConsumer(SubsystemComponents.Drivetrain.DT_LEFT::set), SubsystemComponents.Drivetrain.DT_RIGHT::set);
+        drivetrain = new TankDrivetrain(new InvertedConsumer(SubsystemComponents.Drivetrain.DRIVETRAIN_LEFT::set), SubsystemComponents.Drivetrain.DRIVETRAIN_RIGHT::set);
         drivetrain.setDefaultCommand(new DriveArcade(drivetrain,()->oi.getLeftJoystickY(),()->oi.getRightJoystickX()));
 
     }
