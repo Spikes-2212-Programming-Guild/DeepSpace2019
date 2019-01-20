5e79d2b884159cfe98d02bc3e7f71aae554609e7
YuvalLevy1 <Yuval213711062>
2019-01-20 20:59:45 +0200

    test commit

--- src/main/java/frc/robot/SubsystemConstants.java
+++ src/main/java/frc/robot/SubsystemConstants.java
@@ -10,9 +10,10 @@ public class SubsystemConstants {
 	}
 
 	public static class Lift {
-        public static final Supplier<Double> UP_SPEED = ConstantHandler.addConstantDouble("Lift Up Speed", 0.9);
-        public static final Supplier<Double> STAYING_SPEED = ConstantHandler.addConstantDouble("Lift Staying Speed",
-                0.2);
+        public static final Supplier<Double> UP_SPEED = ConstantHandler.
+				addConstantDouble("Lift Up Speed", 0.9);
+        public static final Supplier<Double> STAYING_SPEED = ConstantHandler.
+				addConstantDouble("Lift Staying Speed",0.2);
         public static final Supplier<Double> DOWN_SPEED = ConstantHandler
                 .addConstantDouble("Lift First Down Speed", -0.2);
 	}
