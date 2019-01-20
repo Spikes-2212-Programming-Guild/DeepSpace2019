680440d94fb5123ed782c815a7c35abecfb51dfb
YuvalLevy1 <Yuval213711062>
2019-01-20 18:51:49 +0200

    reformatted SubsystemConstants

--- src/main/java/frc/robot/SubsystemConstants.java
+++ src/main/java/frc/robot/SubsystemConstants.java
@@ -5,9 +5,12 @@ import com.spikes2212.dashboard.ConstantHandler;
 import java.util.function.Supplier;
 
 public class SubsystemConstants {
-    public static interface  Drivetrain{
-        public static final Supplier<Double> ORIENTATION_KP = ConstantHandler.addConstantDouble("drivetrain - oriantation kp", 0.7);
-        public static final Supplier<Double> ORIENTATION_KI = ConstantHandler.addConstantDouble("drivetrain - oriantation ki", 0.01);
-        public static final Supplier<Double> ORIENTATION_KD = ConstantHandler.addConstantDouble("drivetrain - oriantation kd", 0.1);
+    public static interface Drivetrain {
+        public static final Supplier<Double> ORIENTATION_KP = ConstantHandler
+                .addConstantDouble("drivetrain - oriantation kp", 0.7);
+        public static final Supplier<Double> ORIENTATION_KI = ConstantHandler
+                .addConstantDouble("drivetrain - oriantation ki", 0.01);
+        public static final Supplier<Double> ORIENTATION_KD = ConstantHandler
+                .addConstantDouble("drivetrain - oriantation kd", 0.1);
     }
 }
