dbe69ec8c7170be27a4b2906dd92dbdbab85d9d0
YuvalLevy1 <Yuval213711062>
2019-01-20 20:01:09 +0200

    used only in the necessary imports.

--- src/main/java/frc/robot/SubsystemComponents.java
+++ src/main/java/frc/robot/SubsystemComponents.java
@@ -2,23 +2,16 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PWMTalonSRX;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.*;
 
 public class SubsystemComponents {
     public static class Drivetrain {
         public static final SpeedControllerGroup DRIVETRAIN_LEFT = new SpeedControllerGroup(
                 new WPI_TalonSRX(RobotMap.CAN.MOTOR_LEFT1), new WPI_TalonSRX(RobotMap.CAN.MOTOR_LEFT2));
 
-		public static final SpeedControllerGroup DRIVETRAIN_RIGHT = new SpeedControllerGroup(
-				new WPI_TalonSRX(RobotMap.CAN.MOTOR_RIGHT1), new WPI_TalonSRX(RobotMap.CAN.MOTOR_RIGHT2));
-
-	  public static final Encoder DRIVETRAIN_ENCODER_RIGHT = new Encoder(RobotMap.DIO.DRIVETRAIN_ENCODER_RIGHT1,RobotMap.DIO.DRIVETRAIN_ENCODER_RIGHT2);
-	  public static final Encoder DRIVETRAIN_ENCODER_LEFT = new Encoder(RobotMap.DIO.DRIVETRAIN_ENCODER_LEFT1,RobotMap.DIO.DRIVETRAIN_ENCODER_LEFT2);
-
-    }    
+        public static final SpeedControllerGroup DRIVETRAIN_RIGHT = new SpeedControllerGroup(
+                new WPI_TalonSRX(RobotMap.CAN.MOTOR_RIGHT1), new WPI_TalonSRX(RobotMap.CAN.MOTOR_RIGHT2));
+    }
 
     public static class Lift {
         public static final SpeedControllerGroup LIFT_MOTORS = new SpeedControllerGroup(
