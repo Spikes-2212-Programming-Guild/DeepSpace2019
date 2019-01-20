4551de184a01becaf66d00f9b6dd6c50abb49ba4
YuvalLevy1 <Yuval213711062>
2019-01-20 20:22:50 +0200

    fixed encoder's ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -18,8 +18,8 @@ public class RobotMap {
 	public interface DIO {
 	public static final int DRIVETRAIN_ENCODER_RIGHT1 = 0;
 	public static final int DRIVETRAIN_ENCODER_RIGHT2 = 1;
-	public static final int DRIVETRAIN_ENCODER_LEFT1 = 1;
-	public static final int DRIVETRAIN_ENCODER_LEFT2 = 1;
+	public static final int DRIVETRAIN_ENCODER_LEFT1 = 3;
+	public static final int DRIVETRAIN_ENCODER_LEFT2 = 2;
 	}
 
 	public static interface PWM {
