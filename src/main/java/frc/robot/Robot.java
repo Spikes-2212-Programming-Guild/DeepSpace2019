9c7c27f494169a7203e81953c7a4d1999046d22a
YuvalLevy1 <Yuval213711062>
2019-01-20 16:58:38 +0200

    added the lift's constructor.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,20 @@ public class Robot extends TimedRobot {
         oi = new OI();
         drivetrain = new TankDrivetrain(new InvertedConsumer(SubsystemComponents.Drivetrain.DRIVETRAIN_LEFT::set), SubsystemComponents.Drivetrain.DRIVETRAIN_RIGHT::set);
         drivetrain.setDefaultCommand(new DriveArcade(drivetrain, () -> oi.getLeftJoystickY(), () -> oi.getRightJoystickX()));
+        lift = new BasicSubsystem(SubsystemComponents.Lift.LIFT_MOTORS::set, (Double speed) -> {
+            if (speed == 0) // The lift can always move with 0.
+                return true;
+            // Returns false if the lift tries to move up when its in its upper
+            // limit.
+            if (SubsystemComponents.Lift.LIMIT_UP.get() && speed > SubsystemConstants.Lift.STAYING_SPEED.get())
+                return false;
+            // Returns false if the lift tries to move down when its in its
+            // lower limit.
+            if (SubsystemComponents.Lift.LIMIT_DOWN.get() && speed < SubsystemConstants.Lift.STAYING_SPEED.get())
+                return false;
+            return true;
+        });
+
     }
 
 
--- src/main/java/frc/robot/SubsystemConstants.java
+++ src/main/java/frc/robot/SubsystemConstants.java
@@ -13,12 +13,13 @@ public class SubsystemConstants {
 
     public static class Lift {
         public static final Supplier<Double> STAYING_SPEED = ConstantHandler.addConstantDouble("Lift Staying Speed",
-                0.1);
+                0.2);
         public static final Supplier<Double> UP_SPEED = ConstantHandler.addConstantDouble("Lift up Speed",
-                0.1);
+                0.9);
+        public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Lift down Speed",
+                0.9);
     }
-    public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Lift down Speed",
-            0.1);
+
 
     public static class Arm {
         public static final Supplier<Double> ARM_SPEED = ConstantHandler.addConstantDouble("Arm Speed",
